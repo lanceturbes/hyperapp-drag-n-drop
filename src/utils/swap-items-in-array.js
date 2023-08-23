c47a056e3456fa4a3bca2851fe4c366577a4ae4c
Lance Turbes <lturbes@mahalobanking.com>
2023-08-22 23:05:28 -0700

    chore: remove extra console log

--- src/utils/swap-items-in-array.js
+++ src/utils/swap-items-in-array.js
@@ -1,5 +1,4 @@
 export default function swapItemsInArray(array, indexA, indexB) {
-  console.log(array);
   const items = array.slice();
   const itemA = items[indexA];
   items[indexA] = items[indexB];
